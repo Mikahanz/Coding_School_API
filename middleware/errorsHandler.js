aaf74fa4e8ee5d1a9e74e10febb46d48200847e1
Michael Hanzel <41654851+Mikahanz@users.noreply.github.com>
2021-03-03 12:00:18 -0500

    Mongoose error handling 2

--- middleware/errorsHandler.js
+++ middleware/errorsHandler.js
@@ -4,11 +4,11 @@ import ErrorResponse from '../utils/errorResponse.js';
 // ERROR HANDLER MIDDLEWARE - handle all the next(error)
 const errorsHandler = (err, req, res, next) => {
   // log to console for dev ---------------------------------
-  console.log(chalk.red(err));
+  console.log(err);
   console.log(chalk.yellow(`The Error Name ->> ${err.name}`));
-  //console.log(error);
 
   let error = { ...err };
+  //console.log(error);
 
   // Add property 'message' to error
   error.message = err.message;
@@ -17,6 +17,18 @@ const errorsHandler = (err, req, res, next) => {
   if (err.name === 'CastError') {
     const message = `Resource not found due to Invalid ObjectId -> ${err.value}`;
     error = new ErrorResponse(message, 404); // ErrorResponse is custom ErrorResponse Class Extended from Error Interface
+  }
+
+  // Mongoose duplicate key (MongoError)
+  if (error.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   // Response to client
