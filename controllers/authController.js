5e36ab16c474a9b6aa5a8a0f7a32644359df6799
Michael Hanzel <41654851+Mikahanz@users.noreply.github.com>
2021-03-10 11:46:26 -0500

    Forgot password - send email using mailtrap and nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 import SchoolModel from '../models/SchoolModel.js';
 import ErrorResponse from '../utils/errorResponse.js';
 import UserModel from '../models/UserModel.js';
+import sendEmail from '../utils/sendEmail.js';
 
 // @desc POST Register new user
 // @route POST /api/v1/auth/register
@@ -74,6 +75,31 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken();
 
   await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent' });
+  } catch (error) {
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse(`Email could not be sent`, 500));
+  }
 
   res.status(200).json({
     success: true,
