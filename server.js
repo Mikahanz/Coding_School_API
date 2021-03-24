1efaebb74a8f93b599c14826b694125f4d0cbe3c
Michael Hanzel <41654851+Mikahanz@users.noreply.github.com>
2021-03-24 18:22:16 -0400

    fix stuff

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/public')));
 
-  app.get('*', (req, res) =>
+  app.get('/', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
   );
 }
